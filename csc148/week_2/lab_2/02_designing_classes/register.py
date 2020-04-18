ebf0b8f86e727bc308f9dea4f057bf4a7df9f207
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-18 17:37:45 -0600

    fix: Add correct solution 'Task 2.4, Lab 2, CSC 148'

--- csc148/week_2/lab_2/02_designing_classes/register.py
+++ csc148/week_2/lab_2/02_designing_classes/register.py
@@ -1,3 +1,82 @@
+class Runner:
+    """A runner for the race
+
+    === Attributes ===
+    name: the name of runner.
+    email: the email of runner.
+    speed_category: speed category runner is racing in
+
+    === Sample Usage ===
+    Create a runner:
+    >>> runner = Runner('Gerhard', 'gerhard@gmail.com','lt30')
+    >>> runner.name
+    'Gerhard'
+    >>> runner.email
+    'gerhard@gmail.com'
+    >>> runner.speed_category
+    'lt30'
+    """
+    name: str
+    email: str
+    speed_category: str
+
+    def __init__(self, name: str, email: str, speed_category: str) -> None:
+        """Initialize runner
+
+        Precondition: <name> != ''
+        Precondition: <email> != ''
+        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40','']
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.name
+        'Gerhard'
+        >>> runner.email
+        'gerhard@gmail.com'
+        >>> runner.speed_category
+        'lt30'
+        """
+        pass
+
+    def edit_email(self, email: str) -> None:
+        """Edits runner email information
+
+        Precondition: <email> != ''
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.email
+        'gerhard@gmail.com'
+        >>> runner.edit_email('gerhard_2@gmail.com')
+        >>> runner.email
+        'gerhard_2@gmail.com'
+        """
+        pass
+
+    def edit_category(self, speed_category: str) -> None:
+        """Edits runner speed category information
+
+        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40']
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.speed_category
+        'lt30'
+        >>> runner.edit_category('lt20')
+        >>> runner.speed_category
+        'lt20'
+        """
+
+    def withdraw(self) -> None:
+        """Withdraws runner from race
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.speed_category
+        'lt30'
+        >>> runner.withdraw()
+        >>> runner.speed_category
+        ''
+        """
+        pass
+
+
 class Race:
     """Race Registry
 
@@ -38,6 +117,7 @@ class Race:
     >>> runners[1].name
     'Tom'
     """
+    runners: List[Runner]
 
     def __init__(self) -> None:
         """Initializes race registry
@@ -93,82 +173,6 @@ class Race:
         >>> fetched_runner_2 = r.get_runners('Toni')
         >>> fetched_runner_2
         None
-        """
-        pass
-
-
-class Runner:
-    """A runner for the race
-
-    === Attributes ===
-    name: the name of runner.
-    email: the email of runner.
-    speed_category: speed category runner is racing in
-
-    === Sample Usage ===
-    Create a runner:
-    >>> runner = Runner('Gerhard', 'gerhard@gmail.com','lt30')
-    >>> runner.name
-    'Gerhard'
-    >>> runner.email
-    'gerhard@gmail.com'
-    >>> runner.speed_category
-    'lt30'
-    """
-
-    def __init__(self, name: str, email: str, speed_category: str) -> None:
-        """Initialize runner
-
-        Precondition: <name> != ''
-        Precondition: <email> != ''
-        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40','']
-
-        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
-        >>> runner.name
-        'Gerhard'
-        >>> runner.email
-        'gerhard@gmail.com'
-        >>> runner.speed_category
-        'lt30'
-        """
-        pass
-
-    def edit_email(self, email: str) -> None:
-        """Edits runner email information
-
-        Precondition: <email> != ''
-
-        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
-        >>> runner.email
-        'gerhard@gmail.com'
-        >>> runner.edit_email('gerhard_2@gmail.com')
-        >>> runner.email
-        'gerhard_2@gmail.com'
-        """
-        pass
-
-    def edit_category(self, speed_category: str) -> None:
-        """Edits runner speed category information
-
-        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40']
-
-        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
-        >>> runner.speed_category
-        'lt30'
-        >>> runner.edit_category('lt20')
-        >>> runner.speed_category
-        'lt20'
-        """
-
-    def withdraw(self) -> None:
-        """Withdraws runner from race
-
-        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
-        >>> runner.speed_category
-        'lt30'
-        >>> runner.withdraw()
-        >>> runner.speed_category
-        ''
         """
         pass
 
