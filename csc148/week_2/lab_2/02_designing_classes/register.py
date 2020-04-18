e1667dad98a3af150dc3e07d40cb924e687f89d8
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-18 17:06:13 -0600

    feat: Add solution 'Task 2.4, Lab 2, CSC 148'

--- csc148/week_2/lab_2/02_designing_classes/register.py
+++ csc148/week_2/lab_2/02_designing_classes/register.py
@@ -32,23 +32,69 @@ class Race:
     'lt30'
 
     Get all runners in a speed category:
-    >>> r.get_runners('lt30')
-    ['Gerhard','Tom']
+    >>> runners = r.get_runners('lt30')
+    >>> runners[0].name
+    'Gerhard'
+    >>> runners[1].name
+    'Tom'
     """
 
     def __init__(self) -> None:
+        """Initializes race registry
+
+        >>> r = Race()
+        >>> r.runners()
+        []
+        """
         pass
 
-    def register(self, runner: Runner, category: str) -> None:
+    def register(self, runner: Runner) -> None:
+        """Registers runner to race
+
+        >>> r = Race()
+        >>> runner = Runner('Gerhard','gerhard@gmail.com','lt30')
+        >>> r.register(runner)
+        >>> r.runners[0].name
+        'Gerhard'
+        """
         pass
 
     def get_runners(self, category: str) -> None:
+        """Returns list of runners in race category
+
+        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40','']
+
+        >>> runner_1 = Runner('Gerhard','gerhard@gmail.com', 'lt40')
+        >>> r.register(runner_1)
+        >>> runner_2 = Runner('Tom','tom@gmail.com', 'lt20')
+        >>> r.register(runner_2)
+        >>> runner_3 = Runner('Toni','toni@gmail.com', 'lt20')
+        >>> r.register(runner_3)
+        >>> runners = r.get_runners('lt20')
+        >>> runners[0].name
+        'Tom'
+        >>> runners[1].name
+        'Toni'
+        >>> r.get_runners('lt30')
+        []
+        """
         pass
 
     def get_runner(self, name: str) -> None:
-        pass
+        """Returns runner in race registry
 
-    pass
+        Precondition: <name> != ''
+
+        >>> runner_1 = Runner('Gerhard','gerhard@gmail.com', 'lt40')
+        >>> r.register(runner_1)
+        >>> fetched_runner_1 = r.get_runners('Gerhard')
+        >>> fetched_runner_1.name
+        'Gerhard'
+        >>> fetched_runner_2 = r.get_runners('Toni')
+        >>> fetched_runner_2
+        None
+        """
+        pass
 
 
 class Runner:
@@ -71,12 +117,59 @@ class Runner:
     """
 
     def __init__(self, name: str, email: str, speed_category: str) -> None:
+        """Initialize runner
+
+        Precondition: <name> != ''
+        Precondition: <email> != ''
+        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40','']
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.name
+        'Gerhard'
+        >>> runner.email
+        'gerhard@gmail.com'
+        >>> runner.speed_category
+        'lt30'
+        """
         pass
 
-    def edit(self, email: str, speed_category: str) -> None:
+    def edit_email(self, email: str) -> None:
+        """Edits runner email information
+
+        Precondition: <email> != ''
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.email
+        'gerhard@gmail.com'
+        >>> runner.edit_email('gerhard_2@gmail.com')
+        >>> runner.email
+        'gerhard_2@gmail.com'
+        """
         pass
+
+    def edit_category(self, speed_category: str) -> None:
+        """Edits runner speed category information
+
+        Precondition: <speed_category> in ['lt20','lt30','lt40','gt40']
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.speed_category
+        'lt30'
+        >>> runner.edit_category('lt20')
+        >>> runner.speed_category
+        'lt20'
+        """
 
     def withdraw(self) -> None:
+        """Withdraws runner from race
+
+        >>> runner = Runner('Gerhard', 'gerhard@gmail.com', 'lt30')
+        >>> runner.speed_category
+        'lt30'
+        >>> runner.withdraw()
+        >>> runner.speed_category
+        ''
+        """
         pass
 
 if __name__ == '__main__':
