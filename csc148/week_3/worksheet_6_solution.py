102bb7a78cf3e9ffddb6cd64ad65bccf3a1020b2
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-19 14:29:58 -0600

    feat: Add solution 'Question 8, Worksheet 6 Solution, CSC 148'

--- csc148/week_3/worksheet_6_solution.py
+++ csc148/week_3/worksheet_6_solution.py
@@ -1,3 +1,5 @@
+import random
+from datetime import datetime
 from worksheet_6_starter_code import Vehicle, SuperDuperManager
 
 """
@@ -40,3 +42,20 @@ class Helicopter(Vehicle):
         delta_y = abs(old_y - new_y)
 
         return math.ceil(math.sqrt(delta_x**2 + delta_y**2))
+
+
+"""
+Question 8.2
+"""
+
+ class UnreliableMagicCarpet:
+
+    def __init__(self, initial_fuel: int,
+                    initial_position: Tuple[int, int] = (0,0)) -> None:
+        super().__init__()
+
+        random.seed(datetime.now())
+        self.position = (random.randint(-100,100), random.randint(-100,100))
+
+    def move(self, new_x: int, new_y: int) -> None:
+        self.position = (random.randint(-100,100), random.randint(-100,100))
