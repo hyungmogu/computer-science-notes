376038b01919c4965f581a90699179233e1abe00
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-19 13:09:28 -0600

    feat: Add progress #2 'Question 8, Worksheet 6 Solution, CSC 148'

--- csc148/week_3/worksheet_6_solution.py
+++ csc148/week_3/worksheet_6_solution.py
@@ -19,3 +19,24 @@ class Car(Vehicle):
         delta_y = abs(old_y - new_y)
 
         return delta_x + delta_y
+
+
+"""
+Question 8.1
+"""
+
+class Helicopter(Vehicle):
+
+    def __init__(self, initial_fuel: int,
+                    initial_position: Tuple[int, int] = (0,0)) -> None:
+        super().__init__()
+
+    def fuel_needed(self, new_x: int, new_y: int) -> int:
+
+        old_x = self.position[0]
+        old_y = self.position[1]
+
+        delta_x = abs(old_x - new_x)
+        delta_y = abs(old_y - new_y)
+
+        return math.ceil(math.sqrt(delta_x**2 + delta_y**2))
