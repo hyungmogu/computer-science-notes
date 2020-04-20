b41350055e5c7b642af4b8e67a3130e1cc597508
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-19 21:21:34 -0600

    fix: Add correct solution 'Question 4, Worksheet 7 Solution, CSC 148'

--- csc148/week_3/worksheet_7_q4_solution.py
+++ csc148/week_3/worksheet_7_q4_solution.py
@@ -1,23 +1,14 @@
 def get_group_score(group):
-    """Evaluates the group score
-
-        Precondition: len(group) == 4
-    """
-    n = 4
-
-    max_year = get_max_year(group)
-    min_year = get_min_year(group)
+    """Evaluates the list of similarities"""
     similarity_list = []
 
     i = 0
 
-    while i < 4:
-        j = 0
-        while j < 4:
+    while i < len(group): # <- correct solution
+        j = i + 1 # <- correct solution
+        while j < len(group): # <- correct solution
             # find the scaled distance
-            scaled_distance = 0
-            if max_year != min_year:
-                scaled_distance = abs(group[i]['year'] - group[j]['year']) / float(max_year - min_year)
+            scaled_distance = abs(group[i]['year'] - group[j]['year']) / 5.0 # <- correct solution
 
             # find the similarity
             similarity = 1 - scaled_distance
@@ -28,54 +19,31 @@ def get_group_score(group):
             j += 1
         i += 1
 
-    # find the average
-    average = float(sum(similarity_list))/len(similarity_list)
-    return average.as_integer_ratio()
-
-def get_max_year(group):
-    """returns max value of year in group"""
-
-    max_value = -1
-
-    for student in group:
-        max_value = max(student['year'], max_value)
-
-    return max_value
-
-def get_min_year(group):
-    """returns min value of year in group"""
-
-    min_value = 100 # this is impossible value
-
-    for student in group:
-        min_value = min(student['year'], min_value)
-
-    return min_value
+    return similarity_list
 
 
 if __name__ == '__main__':
     group_1 = [{'name': 'Primya', 'year': 3},
+    {'name': 'Alain', 'year': 2},
     {'name': 'Zoe', 'year': 3},
-    {'name': 'Francesco', 'year': 3},
+    {'name': 'Francesco', 'year': 3}]
+
+    group_2 = [{'name': 'Mohammed', 'year': 4},
+    {'name': 'Xiaoyuan', 'year': 5},
+    {'name': 'Rohit', 'year': 2},
     {'name': 'Yimin', 'year': 3}]
 
-    group_2 = [{'name': 'Primya', 'year': 1},
-    {'name': 'Zoe', 'year': 1},
-    {'name': 'Francesco', 'year': 2},
-    {'name': 'Yimin', 'year': 2}]
-
-    group_3 = [{'name': 'Primya', 'year': 5},
-    {'name': 'Zoe', 'year': 5},
-    {'name': 'Francesco', 'year': 4},
-    {'name': 'Yimin', 'year': 3}]
+    group_3 = [{'name': 'Grace', 'year': 5},
+    {'name': 'Claire', 'year': 1},
+    {'name': 'Kai', 'year': 1}]
 
 
-    score_1 =  get_group_score(group_1)
-    print(score_1)
+    list_1 =  get_group_score(group_1)
+    print(list_1)
 
-    score_2 =  get_group_score(group_2)
-    print(score_2)
+    list_2 =  get_group_score(group_2)
+    print(list_2)
 
-    score_3 =  get_group_score(group_3)
-    print(score_3)
+    list_3 =  get_group_score(group_3)
+    print(list_3)
 
