ddd24cc59d629d251cb42b196d997f34ea34d1bd
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-29 03:15:39 +0900

    feat: Add solution 'Task 5, Assignment 1, CSC 148'

--- csc148/week_6/assignment_1/05_complete_the_answer_class/tests.py
+++ csc148/week_6/assignment_1/05_complete_the_answer_class/tests.py
@@ -34,16 +34,19 @@ class TestAnswer:
         answer1 = Answer("E")
         answer2 = Answer(1)
         answer3 = Answer(True)
+        answer4 = Answer(["A","B"])
 
         expected = False
 
         result1 = answer1.is_valid(multiple_choice_question)
         result2 = answer2.is_valid(multiple_choice_question)
         result3 = answer3.is_valid(multiple_choice_question)
+        result4 = answer4.is_valid(multiple_choice_question)
 
         assert expected == result1
         assert expected == result2
         assert expected == result3
+        assert expected == result4
 
     def test_is_valid_method_should_return_True_for_all_valid_answers_to_numeric_question(self,numeric_question):
         expected = True
@@ -58,7 +61,7 @@ class TestAnswer:
         answer2 = Answer(11)
         answer3 = Answer(True)
         answer4 = Answer("A")
-        answer5 = Answer(['a','b'])
+        answer5 = Answer(["A","B"])
 
         expected = False
 
@@ -67,6 +70,64 @@ class TestAnswer:
         result3 = answer3.is_valid(numeric_question)
         result4 = answer4.is_valid(numeric_question)
         result5 = answer5.is_valid(numeric_question)
+
+        assert expected == result1
+        assert expected == result2
+        assert expected == result3
+        assert expected == result4
+        assert expected == result5
+
+    def test_is_valid_method_should_return_True_for_all_valid_answers_to_yesno_question(self,yes_no_question):
+        expected = True
+
+        for value in [True, False]:
+            answer = Answer(value)
+            result = answer.is_valid(yes_no_question)
+            assert expected == result
+
+    def test_is_valid_method_should_return_False_for_invalid_answers_to_yesno_question(self,yes_no_question):
+        answer1 = Answer(0)
+        answer2 = Answer(1)
+        answer3 = Answer(11)
+        answer4 = Answer("A")
+        answer5 = Answer(["A","B"])
+
+        expected = False
+
+        result1 = answer1.is_valid(yes_no_question)
+        result2 = answer2.is_valid(yes_no_question)
+        result3 = answer3.is_valid(yes_no_question)
+        result4 = answer4.is_valid(yes_no_question)
+        result5 = answer5.is_valid(yes_no_question)
+
+        assert expected == result1
+        assert expected == result2
+        assert expected == result3
+        assert expected == result4
+        assert expected == result5
+
+    def test_is_valid_method_should_return_True_for_all_valid_answers_to_checkbox_question(self,checkbox_question):
+        answer = Answer(["A","B","C"])
+
+        expected = True
+
+        result = answer.is_valid(checkbox_question)
+        assert expected == result
+
+    def test_is_valid_method_should_return_False_for_invalid_answers_to_checkbox_question(self,checkbox_question):
+        answer1 = Answer(11)
+        answer2 = Answer(True)
+        answer3 = Answer(False)
+        answer4 = Answer("A")
+        answer5 = Answer(["E","F","G"])
+
+        expected = False
+
+        result1 = answer1.is_valid(checkbox_question)
+        result2 = answer2.is_valid(checkbox_question)
+        result3 = answer3.is_valid(checkbox_question)
+        result4 = answer4.is_valid(checkbox_question)
+        result5 = answer5.is_valid(checkbox_question)
 
         assert expected == result1
         assert expected == result2
