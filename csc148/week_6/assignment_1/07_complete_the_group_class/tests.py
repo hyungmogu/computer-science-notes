ae331a2e1af9fe9236090d2c910876337f80d900
Hyungmo Gu <guhyungm7@gmail.com>
2020-04-30 11:42:14 +0900

    feat: Add files 'Task 7, Assignment 1, CSC 148'

---
+++ csc148/week_6/assignment_1/07_complete_the_group_class/tests.py
@@ -0,0 +1,9 @@
+# TODO: put all your tests in this file (you can delete this line)
+
+import pytest
+import unittest
+from criterion import (HomogeneousCriterion, HeterogeneousCriterion,
+                       LonelyMemberCriterion, InvalidAnswerError)
+from survey import (Question, MultipleChoiceQuestion, NumericQuestion,
+                    YesNoQuestion, CheckboxQuestion, Answer)
+
