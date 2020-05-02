771b904120b62a56d03e85bbbca684c9b6e9663f
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-03 03:42:02 +0900

    feat: Add solution 'Task 10, Assignment 1, CSC 148'

--- csc148/week_6/assignment_1/10_complete_helper_functions_in_grouper_py/tests.py
+++ csc148/week_6/assignment_1/10_complete_helper_functions_in_grouper_py/tests.py
@@ -13,12 +13,15 @@ def lst_1() -> List:
 def lst_2() -> List:
     return ['a', 1, 6.0, False]
 
+@pytest.fixture
+def lst_empty() -> List:
+    return []
+
 class TestSliceListFunction:
-    def test_function_should_return_empty_if_lst_has_length_0(self):
-        lst = []
+    def test_function_should_return_empty_if_lst_has_length_0(self, lst_empty):
         expected = []
 
-        result = slice_list(lst, 0)
+        result = slice_list(lst_empty, 0)
 
         assert expected == result
 
@@ -32,3 +35,20 @@ class TestSliceListFunction:
         assert expected_1 == result_1
         assert expected_2 == result_2
 
+class TestWindowFunction:
+    def test_function_should_return_empty_if_lst_has_length_0(self, lst_empty):
+        expected = []
+
+        result = windows(lst_empty, 0)
+
+        assert expected == result
+
+    def test_function_should_return_correct_value_if_successful(self, lst_1, lst_2):
+        expected_1 = [[3, 4], [4, 6], [6, 2], [2, 3]]
+        expected_2 = [['a', 1, 6.0], [1, 6.0, False]]
+
+        result_1 = windows(lst_1, 2)
+        result_2 = windows(lst_2, 3)
+
+        assert expected_1 == result_1
+        assert expected_2 == result_2
