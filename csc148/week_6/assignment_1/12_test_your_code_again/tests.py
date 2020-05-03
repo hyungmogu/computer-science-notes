55d1c00135fc9b4484c23d732deacf9249feb647
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-03 13:54:16 +0900

    feat: Add progress #2 'Task 12, Assignment 1, CSC 148'

--- csc148/week_6/assignment_1/12_test_your_code_again/tests.py
+++ csc148/week_6/assignment_1/12_test_your_code_again/tests.py
@@ -14,18 +14,40 @@ def student():
     """Initializes a student with name Simon"""
     return Student(1, 'Simon')
 
+@pytest.fixture
+def course():
+    """Initializes a course with name CSC148"""
+    return Course('CSC148')
+
+@pytest.fixture
+def multiple_choice_question() -> MultipleChoiceQuestion:
+    return MultipleChoiceQuestion(1, "Multiplechoice Question", ["A","B","C","D"])
+
+@pytest.fixture
+def numeric_question() -> NumericQuestion:
+    return NumericQuestion(1, "Numeric Question", 1, 10)
+
+@pytest.fixture
+def yes_no_question() -> YesNoQuestion:
+    return YesNoQuestion(1, "YesNo Question")
+
+@pytest.fixture
+def checkbox_question() -> CheckboxQuestion:
+    return CheckboxQuestion(1, "Checkbox Question", ["A","B","C"])
+
 """
-Test for student.__str__
+Student Class
 """
+
 class TestStudent:
-    def test_str_method_should_return_simon_as_student_name(self, student):
+    def test_str_method_should_return_simon_as_student_name(self, student, multiple_choice_question):
         expected = 'Simon'
         result = student.name
         assert expected == result
 
-    def test_set_answer_method_should_return_1_for_the_length_of_answers_keys(self, student):
-        q = Question(2, 'Question')
-        a = Answer('Answer')
+    def test_set_answer_method_should_return_1_for_the_length_of_answers_keys(self, student, multiple_choice_question):
+        q = multiple_choice_question
+        a = Answer('A')
         student.set_answer(q, a)
 
         expected = 1
@@ -34,21 +56,21 @@ class TestStudent:
 
         assert expected == result
 
-    def test_set_method_should_return_2_for_the_key_in_answers(self, student):
-        q = Question(2, 'Question')
-        a = Answer('Answer')
+    def test_set_method_should_return_1_for_the_key_in_answers(self, student, multiple_choice_question):
+        q = multiple_choice_question
+        a = Answer('A')
         student.set_answer(q, a)
 
-        expected = 2
+        expected = 1
 
         result = list(dict.keys(student.answers))[0]
 
         assert expected == result
 
-    def test_set_method_should_return_False_when_student_does_not_have_answer_to_question(self, student):
-        q1 = Question(2, 'Question')
-        q2 = Question(3, 'Question')
-        a1 = Answer('Answer')
+    def test_set_method_should_return_False_when_student_does_not_have_answer_to_question(self, student, multiple_choice_question):
+        q1 = multiple_choice_question
+        q2 = MultipleChoiceQuestion(3, 'Question', ["A","B","C","D"])
+        a1 = Answer('A')
 
         student.set_answer(q1, a1)
         expected = False
@@ -57,15 +79,68 @@ class TestStudent:
 
         assert result == expected
 
-    def test_get_answer_method_should_return_Answer_with_content_answer(self, student):
-        q1 = Question(2, 'Question')
-        a1 = Answer('answer')
+    def test_get_answer_method_should_return_Answer_with_content_A(self, student, multiple_choice_question):
+        q1 = multiple_choice_question
+        a1 = Answer('A')
 
         student.set_answer(q1, a1)
-        expected = 'answer'
+        expected = 'A'
 
         result = student.get_answer(q1).content
 
         assert result == expected
 
 
+"""
+Course Class
+"""
+
+class TestCourse:
+    def test_enroll_students_method_should_cause_students_attribute_should_have_length_of_1(self, course, student):
+        course.enroll_students([student])
+        expected = 1
+
+        result = len(course.students)
+
+        assert expected == result
+
+    def test_enroll_students_method_should_cause_student_in_students_attribute_to_have_name_simon(self, course, student):
+        course.enroll_students([student])
+        expected = 'Simon'
+
+        result = course.students[0].name
+
+        assert expected == result
+
+    def test_enroll_students_method_should_cause_students_attribute_to_have_length_of_1(self, course, student):
+
+        course.enroll_students([student])
+        expected = 1
+
+        course.enroll_students([student])
+        result = len(course.students)
+
+        assert expected == result
+
+    def test_all_answered_method_should_return_True_when_survey_questions_dont_exist(self, course):
+        survey = Survey([])
+        expected = True
+
+        result = course.all_answered(survey)
+
+        assert expected == result
+
+    def test_get_students_method_should_return_all_added_students_in_increasing_order_by_id(self, course):
+        s1 = Student(1, 'John')
+        s2 = Student(2, 'Mary')
+        survey = course.enroll_students([s2,s1])
+        expected = [s1,s2]
+
+        result = course.get_students()
+
+        assert expected == result
+
+
+
+if __name__ == '__main__':
+    pytest.main(['tests.py'])
