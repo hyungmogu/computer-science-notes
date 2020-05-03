9ab17765333e22bbc7450eee3b53acd2df9fbdb5
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-03 13:26:26 +0900

    feat: Add progress 'Task 12, Assignment 1, CSC 148'

--- csc148/week_6/assignment_1/12_test_your_code_again/tests.py
+++ csc148/week_6/assignment_1/12_test_your_code_again/tests.py
@@ -2,3 +2,70 @@
 
 import pytest
 import unittest
+from typing import List
+from course import Course, Student
+from grouper import AlphaGrouper, GreedyGrouper, RandomGrouper, WindowGrouper
+from survey import (Answer, CheckboxQuestion, MultipleChoiceQuestion,
+                    NumericQuestion, Question, Survey, YesNoQuestion,
+                    InvalidAnswerError)
+
+@pytest.fixture
+def student():
+    """Initializes a student with name Simon"""
+    return Student(1, 'Simon')
+
+"""
+Test for student.__str__
+"""
+class TestStudent:
+    def test_str_method_should_return_simon_as_student_name(self, student):
+        expected = 'Simon'
+        result = student.name
+        assert expected == result
+
+    def test_set_answer_method_should_return_1_for_the_length_of_answers_keys(self, student):
+        q = Question(2, 'Question')
+        a = Answer('Answer')
+        student.set_answer(q, a)
+
+        expected = 1
+
+        result = len(list(dict.keys(student.answers)))
+
+        assert expected == result
+
+    def test_set_method_should_return_2_for_the_key_in_answers(self, student):
+        q = Question(2, 'Question')
+        a = Answer('Answer')
+        student.set_answer(q, a)
+
+        expected = 2
+
+        result = list(dict.keys(student.answers))[0]
+
+        assert expected == result
+
+    def test_set_method_should_return_False_when_student_does_not_have_answer_to_question(self, student):
+        q1 = Question(2, 'Question')
+        q2 = Question(3, 'Question')
+        a1 = Answer('Answer')
+
+        student.set_answer(q1, a1)
+        expected = False
+
+        result = student.has_answer(q2)
+
+        assert result == expected
+
+    def test_get_answer_method_should_return_Answer_with_content_answer(self, student):
+        q1 = Question(2, 'Question')
+        a1 = Answer('answer')
+
+        student.set_answer(q1, a1)
+        expected = 'answer'
+
+        result = student.get_answer(q1).content
+
+        assert result == expected
+
+
