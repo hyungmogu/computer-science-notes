046669ab7e7d2c3151ba20fb51e3adfb11f30357
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-14 10:48:32 +0900

    feat: Add note 'Strings in C 6 of 6, Week 5 Notes, CSC 209'

--- csc209/week_5/strings_example_6.c
+++ csc209/week_5/strings_example_6.c
@@ -3,13 +3,20 @@
 
 int main() {
     char s1[30] = "University of C Programming";
-    char *p;
-    p = strchr(s1, 'v');
+    char *p1, *p2;
+    p1 = strchr(s1, 'v');
+    p2 = strstr(s1, "Prog");
 
-    if (p == NULL) {
+    if (p1 == NULL) {
         printf("Character not found\n");
     } else {
-        printf("Character found at index %d\n", (int)(p - s1));
+        printf("Character found at index %d\n", (int)(p1 - s1));
+    }
+
+    if (p2 == NULL) {
+        printf("String not found\n");
+    } else {
+        printf("String found at index %d\n", (int)(p2 - s1));
     }
 
     return 0;
