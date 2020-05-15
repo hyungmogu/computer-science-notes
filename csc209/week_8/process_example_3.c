ae4248735626cef2a1b1fb8507943f8a53c991e1
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-16 08:13:31 +0900

    feat: Add progress 'Process 4 of 8, Week 8 Notes, CSC 209'

---
+++ csc209/week_8/process_example_3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main() {
+    int result;
+    int i,j;
+
+    printf("[%d] Original process (my parent is %d)\n", getpid(), getppid());
+
+    for (i = 0; i < 5; i++) {
+        result = fork();
+
+        if (result == -1) {
+            perror("fork:");
+            exit(1);
+        } else if (result == 0) {
+            for (j = 0; j < 5; j++) {
+                printf("[%d] Child %d %d\n", getpid(), i, j);
+                usleep(100);
+            }
+
+            exit(0);
+        }
+    }
+
+    for(i = 0; i < 5; i++) {
+        pid_t pid;
+        int status;
+        if ((pid = wait(&status)) == -1) {
+            perror("wait");
+        } else {
+            printf("Child %d terminated with %d\n", pid, status);
+        }
+    }
+    printf("[%d] Parent about to terminate\n", getpid());
+    return 0;
+}
