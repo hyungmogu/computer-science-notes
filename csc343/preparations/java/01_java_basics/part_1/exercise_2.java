f2b1ac69b845fc10ad08acef4b6a6eee7fc1963a
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-19 07:25:33 +0900

    feat: Add solution, 'Exercise 2, Part 1, Java Basics, CSC 343 Prep'

---
+++ csc343/preparations/java/01_java_basics/part_1/exercise_2.java
@@ -0,0 +1,51 @@
+// ====== Challenge Task 1 of 4  ======
+
+// Declare a variable that is named the camel-cased version of "first name". Store
+// the user's first name into this new variable using console.readLine.
+
+
+// I have imported java.io.Console for you.  It is a variable called console.
+
+String firstName = console.readLine("Your name is:   ");
+
+
+// ====== Challenge Task 2 of 4  ======
+
+// Declare another variable, naming this one the camel-cased version of "last name".
+// Use console.readLine to store the user's last name into this new variable.
+
+
+// I have imported java.io.Console for you.  It is a variable called console.
+
+String firstName = console.readLine("Your first name is:   ");
+String lastName = console.readLine("Your last name is:   ");
+
+
+// ====== Challenge Task 3 of 4  ======
+
+// Using the console's printf method, display a message that says, "First name: ",
+// followed by the first name that the user has entered.
+
+
+// I have imported java.io.Console for you.  It is a variable called console.
+
+String firstName = console.readLine("Your first name is:   ");
+String lastName = console.readLine("Your last name is:   ");
+
+console.printf("First name: %s", firstName);
+
+
+// ====== Challenge Task 4 of 4  ======
+
+// Using the console's printf method, display a message that says, "First name: ",
+// followed by the first name that the user has entered.
+
+
+// I have imported java.io.Console for you.  It is a variable called console.
+
+String firstName = console.readLine("Your first name is:   ");
+String lastName = console.readLine("Your last name is:   ");
+
+console.printf("First name: %s", firstName);
+console.printf("Last name: %s", lastName);
+
