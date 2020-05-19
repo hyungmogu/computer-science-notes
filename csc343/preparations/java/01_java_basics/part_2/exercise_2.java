d6079a1051ee0e8a4fb5382da02fa4738faa148e
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-20 08:05:40 +0900

    feat: Add solution 'Exercise 2, Part 2, Java Basics, CSC 343 Prep'

---
+++ csc343/preparations/java/01_java_basics/part_2/exercise_2.java
@@ -0,0 +1,76 @@
+"""
+====== Challenge Task 1 of 4  ======
+
+I have placed several errors in this challenge. To fix them you must remove the // to uncomment them and then correct the error.
+
+Uncomment the first line and fix the error. If you do not see the error, click submit and view the Preview pane to view
+the compiler error.
+"""
+
+// Sting firstName = "Gordon";
+String firstName = "Gordon";
+
+// string lastName = "Sumner";
+
+// console.printf("Hello %s", firstName)
+
+// band = console.readLine("Which band?  ");
+
+
+"""
+====== Challenge Task 2 of 4  ======
+
+Now uncomment and correct the second line. If you do not see the error, click submit and use preview tab to see the
+compiler output.
+"""
+
+
+// Sting firstName = "Gordon";
+String firstName = "Gordon";
+
+// string lastName = "Sumner";
+String lastName = "Sumner";
+
+// console.printf("Hello %s", firstName)
+
+// band = console.readLine("Which band?  ");
+
+
+"""
+====== Challenge Task 3 of 4  ======
+
+Uncomment the third line and correct the mistake. If you aren't sure, click the Check Work button and then use the preview
+tab to see the compiler error.
+"""
+
+
+// Sting firstName = "Gordon";
+String firstName = "Gordon";
+
+// string lastName = "Sumner";
+String lastName = "Sumner";
+
+// console.printf("Hello %s", firstName)
+console.printf("Hello %s", firstName);
+
+// band = console.readLine("Which band?  ");
+
+
+"""
+====== Challenge Task 4 of 4  ======
+
+Finally, uncomment the last line and correct it. This one seems to be missing the
+data type declaration at the start of the statement. Press Check Work if you don't
+see the mistake, and then click Preview to see the compiler error.
+"""
+
+// Sting firstName = "Gordon";
+String firstName = "Gordon";
+
+// string lastName = "Sumner";
+String lastName = "Sumner";
+
+console.printf("Hello %s", firstName);
+
+// band = console.readLine("Which band?  ");
+String band = console.readLine("Which band?  ");
