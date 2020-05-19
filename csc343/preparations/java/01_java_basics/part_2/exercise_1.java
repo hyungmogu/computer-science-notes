555bc72a55cbf207a17a5cab6b7db83191a15d3b
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-20 07:34:15 +0900

    feat: Add solution 'Exercise 1, Part 2, Java Basics, CSC 343 Prep'

---
+++ csc343/preparations/java/01_java_basics/part_2/exercise_1.java
@@ -0,0 +1,27 @@
+// ===== Challenge Task 1 of 3 =====
+// Create a new variable called name and accept input from the console using the
+// readLine method.
+
+String name = console.readLine("Enter Name:  ");
+
+
+// ===== Challenge Task 2 of 3 =====
+// Create a new variable to store a past tense verb, so in camel case that is
+// pastTenseVerb. Accept input from the console using the readLine method.
+
+
+String name = console.readLine("Enter Name:  ");
+String pastTenseVerb = console.readLine("Entry Past Tense:    ");
+
+
+// ===== Challenge Task 3 of 3 =====
+// Output a sentence that takes both the name and past tense verb using a single
+// statement. It should look like this:
+
+// *name* really *past tense verb* this coding exercise.
+
+
+String name = console.readLine("Enter Name:  ");
+String pastTenseVerb = console.readLine("Entry Past Tense:    ");
+
+console.printf("%s really %s", name, pastTenseVerb);
