664aca1d8a05e76941262478bfaf2732eb990629
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-21 10:14:49 +0900

    feat: Add progress #2 'Part 3 Notes, Java Basics, CSC 343 Prep'

---
+++ csc343/preparations/java/01_java_basics/part_3/exercise_2.java
@@ -0,0 +1,7 @@
+// ===== Challenge Task 1 of 1 =====
+
+// Create a new int variable that is named answer. Store the integer version of
+// answerToLife in it.
+
+String answerToLife = "42";
+int answer = Integer.parseInt(answerToLife);
