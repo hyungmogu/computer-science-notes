b0565bd9f5d48efd1d77972cccedf95b2a3b406d
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-22 11:11:01 +0900

    feat: Add progress #5 'Part 2 Notes, Java Objects, CSC 343 Prep'

---
+++ csc343/preparations/java/02_java_objects/part_2/lesson_7/Example.java
@@ -0,0 +1,32 @@
+import java.io.Console;
+
+public class Example {
+    public static void main(String[] args) {
+
+        System.out.println("We are making a new PEZ dispenser\n");
+        System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser\n", PezDispenser.MAX_PEZ);
+
+        PezDispenser dispenser = new PezDispenser("Yoda");
+
+        if (dispenser.isEmpty()) {
+            System.out.printf("Dispenser is empty");
+        }
+
+        System.out.printf("The dispenser is %s\n", dispenser.getCharacterName());
+
+        System.out.printf("Filling the dispenser with delicious PEZ...\n");
+        dispenser.fill();
+
+        if (!dispenser.isEmpty()) {
+            System.out.printf("Dispenser is full\n");
+        }
+
+        while (dispenser.dispense()) {
+            System.out.println("Chomp!");
+        }
+
+        if (dispenser.isEmpty()) {
+            System.out.println("Ate all the PEZ");
+        }
+    }
+}
