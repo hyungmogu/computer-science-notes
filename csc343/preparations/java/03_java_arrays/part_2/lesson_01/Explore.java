fded11645945d7398ec59427665ff2acb987affc
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-27 06:23:50 +0900

    feat: Add progress #2 'Part 2 Notes, Java Arrays, CSC 343 Prep'

---
+++ csc343/preparations/java/03_java_arrays/part_2/lesson_01/Explore.java
@@ -0,0 +1,9 @@
+
+public class Explore {
+    public static void main(String[] args) {
+        String[] classmates = {"Ben", "Johnny", "Pasan"};
+        for (String classmate : classmates) {
+            System.out.printf("%s is my class friend\n", classmate);
+        }
+    }
+}
