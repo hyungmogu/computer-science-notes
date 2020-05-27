a0e8d87cbd615a9eeaa095aa43095c8416e950b7
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-27 11:00:54 +0900

    feat: Add progress #4 'Part 3 Notes, Java Arrays, CSC 343 Prep'

---
+++ csc343/preparations/java/03_java_arrays/part_3/lesson_05/Explore.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Explore {
+    public static void main(String[] args) {
+        Arrays.toString(pickLunchSpots("Cafeteria", "Home", "Outside"));
+
+        // Returns ["Ben", "Pasan", "Johnny"]
+    }
+}
+
+
+public String pickLunchSpots(String... spots) {
+    return spots;
+}
