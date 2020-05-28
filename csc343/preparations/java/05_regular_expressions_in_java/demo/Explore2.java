e45818051bdf702be09fcb3f0978ac91f70fa256
Hyungmo Gu <guhyungm7@gmail.com>
2020-05-28 11:01:18 +0900

    feat: Add progress #3 'Notes, Regular Expression in Java, CSC 343 Prep'

--- csc343/preparations/java/05_regular_expressions_in_java/demo/Explore2.java
+++ csc343/preparations/java/05_regular_expressions_in_java/demo/Explore2.java
@@ -1,14 +1,10 @@
 import java.io.Console;
 
-public class Explore1 {
+public class Explore2 {
     public static void main(String[] args) {
-        Console console = System.console();
-        // Your amazing code below
-        String zipCode = "90210";
-        if (zipCode.matches("^\\d{5}(-\\d{4})?$")) {
-        System.out.printf("%s is a valid zip code%n", zipCode);
-        } else {
-        System.out.printf("%s is NOT a valid zip code%n", zipCode);
+        String skills = "JavaScript, HTML, CSS, and Java";
+        for (String skill : skills.split("\\W+(and\\W+)?")) {
+            System.out.printf("Skill : %s \n", skill);
         }
     }
 }
