b02a21f98e4649c05097d5cf2ed7980045e119c9
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-03 06:35:06 +0900

    feat: Add progress 'Part 2 Notes, SQL basics, CSC 343 Prep'

---
+++ csc343/preparations/sql/01_sql_basics/part_2/exercise_1.sql
@@ -0,0 +1,53 @@
+"""
+===== Challenge Task 1 of 5 =====
+
+In this code challenge you have an eCommerce database full of tables that you
+might expect to see in a real database.
+
+Write a SQL query to answer the following question. What are all the products in
+the products table?
+"""
+
+
+SELECT * from products;
+
+"""
+===== Challenge Task 2 of 5 =====
+
+Write the SQL to retrieve all customer information from the users table.
+"""
+
+
+SELECT * from users;
+
+"""
+===== Challenge Task 3 of 5 =====
+
+Select all addresses from the customer_addresses table.
+"""
+
+SELECT * from customer_addresses;
+
+
+"""
+===== Challenge Task 4 of 5 =====
+
+We're using a different database now. Did you know mobile phones have databases?
+Find all the contacts in this smartphone database. All the contacts are in a
+table called phone_book.
+"""
+
+SELECT * from phone_book;
+
+
+
+"""
+===== Challenge Task 5 of 5 =====
+
+In the Yorkshire Division Four in Rugby, the team Hessle RUFC have a website that
+shows their latest matches. Their database holds a results table that stores
+their latest wins and losses. Why not have a look at their latest results
+yourself!?
+"""
+
+SELECT * from results;
