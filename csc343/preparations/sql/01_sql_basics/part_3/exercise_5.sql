05a33fbb7f2146046641186399fec54b67b7ceee
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-03 11:30:30 +0900

    feat: Add progress #8 'Part 3 Notes, SQL basics, CSC 343 Prep'

--- csc343/preparations/sql/01_sql_basics/part_3/exercise_5.sql
+++ csc343/preparations/sql/01_sql_basics/part_3/exercise_5.sql
@@ -9,3 +9,16 @@ Without using the OR keyword, find all products with the price of 7.99, 9.99 or
 """
 
 SELECT * FROM products WHERE price IN (7.99, 9.99, 11.99);
+
+
+"""
+===== Challenge Task 2 of 2 =====
+
+We have an e-commerce database. Inside the users table we have the columns of id,
+username, password, first_name and last_name.
+
+Without using the OR keyword, find all the users with the username of '2spooky4me'
+or 'beard_man'.
+"""
+
+SELECT * FROM users WHERE username IN ('2spooky4me', 'beard_man');
