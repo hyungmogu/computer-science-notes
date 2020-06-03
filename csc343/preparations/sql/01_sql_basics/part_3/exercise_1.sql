aff8bcf3700af6bc824e9758501adaf267915f5b
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-03 10:05:43 +0900

    feat: Add progress #2 'Part 3 Notes, SQL basics, CSC 343 Prep'

---
+++ csc343/preparations/sql/01_sql_basics/part_3/exercise_1.sql
@@ -0,0 +1,11 @@
+"""
+===== Challenge Task 1 of 3 =====
+
+In our e-commerce database we have a users table with the columns id, username,
+password, first_name and last_name.
+
+Write a SQL query that retrieves the first and last names only where the username
+is equal to 'wig_lady'.
+"""
+
+
