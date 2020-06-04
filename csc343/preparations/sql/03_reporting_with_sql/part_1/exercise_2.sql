1dd2dea146941ec4a270eeee80aea585cd800469
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-05 02:20:19 +0900

    feat: Add progress #3 'Part 1 Notes, Reporting with SQL, CSC 343 Prep'

---
+++ csc343/preparations/sql/03_reporting_with_sql/part_1/exercise_2.sql
@@ -0,0 +1,23 @@
+"""
+===== Challenge Task 1 of 2 =====
+
+We're using the library database again. There's a books table. There's a title,
+author, genre and first_published column.
+
+Write a query to obtain the first 5 books in the Fantasy genre ordered by the year
+released. Oldest first. Select all columns.
+"""
+
+SELECT * FROM books WHERE genre = 'Fantasy' ORDER BY first_published ASC LIMIT 5;
+
+
+"""
+===== Challenge Task 2 of 2 =====
+
+We're still using the library database with the books table. There's a title,
+author, genre and first_published column.
+
+Find the earliest Science Fiction book in our library. Only one result should be
+returned. All columns should be selected.
+"""
+
