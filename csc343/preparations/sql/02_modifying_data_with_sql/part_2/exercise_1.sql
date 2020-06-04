761313436a030dd956e0de65ab9424ec9f746b07
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-04 10:48:39 +0900

    feat: Add progress #3 'Part 2 Notes, Modifying Data with SQL, CSC 343 Prep'

---
+++ csc343/preparations/sql/02_modifying_data_with_sql/part_2/exercise_1.sql
@@ -0,0 +1,24 @@
+"""
+===== Challenge Task 1 of 2 =====
+
+We have an eCommerce database and it has a products table. It has the columns
+id, name, description and price.
+
+The warehouse is closing down. There's a clearance sale and all products need to
+be priced 0.99.
+"""
+
+UPDATE products SET price = 0.99;
+
+
+"""
+===== Challenge Task 2 of 2 =====
+
+Now we're using a database from a smartphone. It has a phone_book table. It has
+the columns id, first_name, last_name and phone.
+
+Update all contacts to have the first name of 'Mystery' and last name of
+'Person'.
+"""
+
+UPDATE phone_book SET first_name = "Mystery", last_name = "Person";
