f075a8419437f5bafd04359e0ad7030c941f8c1e
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-08 09:59:30 +0900

    feat: Add notes ‘Part 5 notes, Querying Relational Databases, CSC 343 Prep'

--- csc343/preparations/teamtreehouse/sql/04_querying_relational_databases/part_5/exercise_1.sql
+++ csc343/preparations/teamtreehouse/sql/04_querying_relational_databases/part_5/exercise_1.sql
@@ -40,3 +40,56 @@ has a VegetableID and Name column.
 Create a list of all fruits and vegetables starting with the letters A through K . In other words all fruit and vegetables
 that don't start with the letter L to Z.
 """
+
+SELECT Name FROM Fruit
+		UNION ALL
+SELECT Name FROM Vegetable
+	ORDER BY Name;
+
+
+"""
+===== Challenge Task 4 of 6 =====
+
+There are two tables Fruit and Vegetable table. The Fruit table has a FruitID
+and a Name column and the Vegetable table has a VegetableID and Name column.
+
+Create an alphabetical list of produce that is considered both a fruit and a
+vegetable.
+"""
+
+SELECT Name FROM Fruit
+		INTERSECT
+SELECT Name FROM Vegetable
+	ORDER BY Name;
+
+
+
+"""
+===== Challenge Task 5 of 6 =====
+
+There are two tables Fruit and Vegetable table. The Fruit table has a FruitID and
+a Name column and the Vegetable table has a VegetableID and Name column.
+
+Create an alphabetical list of fruits that are NOT considered a vegetable.
+"""
+
+SELECT Name FROM Fruit
+		EXCEPT
+SELECT Name FROM Vegetable
+	ORDER BY Name;
+
+
+"""
+===== Challenge Task 6 of 6 =====
+
+There are two tables Fruit and Vegetable table. The Fruit table has a FruitID
+and a Name column and the Vegetable table has a VegetableID and Name column.
+
+Create an alphabetical list of vegetables that are NOT considered a fruit.
+"""
+
+
+SELECT Name FROM Vegetable
+		EXCEPT
+SELECT Name FROM Fruit
+	ORDER BY Name;
