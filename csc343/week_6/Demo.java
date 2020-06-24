a447c4e5f5826b8e4fe8fc3a5601199e7aa16021
Hyungmo Gu <guhyungm7@gmail.com>
2020-06-25 07:12:55 +0900

    feat: Add progress #2 ‘Question 3.b, Worksheet 8 Solution, CSC 343’

---
+++ csc343/week_6/Demo.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[ ] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Hello Java");
+        String minSpeedRaw = myObj.nextLine();
+        System.out.println(minSpeedRaw);
+
+        System.out.println("Hello Java 2");
+        String minSpeedRaw2 = myObj.nextLine();
+        System.out.println(minSpeedRaw2);
+    }
+}
