b7a723d09c9ae33f6c59bf7983fc81f5d2c839c9
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-17 06:10:49 +0900

    feat: Add solution ‘Question 13, Worksheet 2 solution, CSC 369’

---
+++ csc369/week_1/worksheet_2/question_13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // fork, write, close, excel
+#include <assert.h> // assert
+#include <time.h>   // delay
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    int rc = fork();
+    assert(rc > -1);
+    if (rc == 0) {
+        // child
+        // Comment the rest of exec commands to continue
+        close(STDOUT_FILENO);
+        printf("I am a child\n");
+    } else {
+        // parent
+        int rc_wait = waitpid(rc, NULL, 0);
+        printf("I am a parent\n");
+    }
+    return 0;
+}
