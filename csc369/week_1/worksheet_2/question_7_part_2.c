91d24c59dd6b442b354866f78ec59c3d1f833d2a
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-16 13:39:08 +0900

    feat: Add solution ‘Question 7, Worksheet 2 solution, CSC 369’

---
+++ csc369/week_1/worksheet_2/question_7_part_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+// I need to write a program that calls fork()
+// Requirements
+//  - Before calling fork(), create a variable and set its value to something (e.g 100)
+
+int main(int argc, char *argv[]) {
+    int x = 1000;
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    } else if (rc == 0){
+        printf("hello, I am child (pid: %d)\n", (int) getpid());
+        x = 20;
+        printf("value of x is: %d\n", x);
+        printf("---------");
+    } else {
+        printf("hello, I am parent %d (pid: %d)\n",rc, (int) getpid());
+        x = 30;
+        printf("value of x is: %d\n", x);
+        printf("---------");
+    }
+
+    return 0;
+}
