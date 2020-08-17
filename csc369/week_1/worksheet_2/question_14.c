c25ab54290e8d89b6e2b07ee3cc0b7c1042687e6
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-17 09:04:52 +0900

    feat: Add progress #4 ‘Question 14, Worksheet 2 solution, CSC 369’

--- csc369/week_1/worksheet_2/question_14.c
+++ csc369/week_1/worksheet_2/question_14.c
@@ -8,14 +8,23 @@
 int main(int argc, char *argv[]) {
     int fd[2], rc, rc2;
 
+    // create pipe
+    int p1 = pipe(fd);
+    assert(p1 > -1);
+
     // Create child process 1
     rc = fork();
     assert(rc > -1);
     if (rc == 0) {
         // Child
+        // close standard input (STDIN_FILENO)
+        close(STDIN_FILENO);
+        // duplicate the input end of pipe (fd[1])
+        dup(fd[1]);
+        // close unused pipe ends
+        close(fd[0]);
+        close(fd[1]);
         // establish the output of this process as the input for pipe
-        close(STDOUT_FILENO);
-        printf("I am a child\n");
         return 0;
     }
 
@@ -24,6 +33,11 @@ int main(int argc, char *argv[]) {
     assert(rc2 > -1);
     if (rc == 0) {
         // Child
+        // close standard output (STDOUT_FILENO)
+        close(STDOUT_FILENO);
+        // duplicate the output end of pipe (fd[0])
+        dup(fd[0]);
+        // duplicate and close unused pipe ends
         // establish the input of this process as the output for pipe
         close(STDOUT_FILENO);
         printf("I am a child\n");
