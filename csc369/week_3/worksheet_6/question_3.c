8434ed6ae1ec9d785633e01b1418834b519e7128
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-20 06:33:15 +0900

    feat: Add progress #4 ‘Question 3, Worksheet 6 solution, CSC 369’

--- csc369/week_3/worksheet_6/question_3.c
+++ csc369/week_3/worksheet_6/question_3.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    // get arguments
 
+    // create array
+
+    // indefinitely traverse through the array, touching elements
+
+    // while traversing, the following should be displayed
+    //  Current index:
+    //  Time:
+    //  Amount of memory allocated:
+    //  Press Cmd + Z to kill program
 }
