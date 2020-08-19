9f4514ab7fd27ae2e0c56dd437cca683ac6258d5
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-19 14:31:30 +0900

    feat: Add solution ‘Question 9, Review 4 solution, CSC 209’

--- csc209/week_4/question_9.c
+++ csc209/week_4/question_9.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 
+void pay_amount(int dollars, int *twenties, int *tens,
+                int *fives, int *ones);
+
 int main(void) {
 
-    int money = 0;
-    int bills[] = {20, 10, 5, 1};
+    int dollars, twenties, tens, fives, ones;
 
     printf("Enter a dollar amount: ");
-    scanf("%d", &money);
+    scanf("%d", &dollars);
+
+    pay_amount(dollars, &twenties, &tens, &fives, &ones);
+
+    printf("$20 bills: %d\n", twenties);
+    printf("$10 bills: %d\n", tens);
+    printf("$5 bills: %d\n", fives);
+    printf("$1 bills: %d\n", ones);
+
+    return 0;
+}
+
+void pay_amount(int dollars, int *twenties, int *tens,
+                int *fives, int *ones) {
+
+    int bills[] = {20, 10, 5, 1};
 
     for (int i = 0; i < 4; i++) {
         int bill = bills[i];
-        int amt = money % bill;
-        printf("$%d bills: %d", bill, amt);
-        money -= amt * bill;
+        int amt = dollars / bill;
 
+        if (bill == 20) {
+            *twenties = amt;
+        } else if (bill == 10) {
+            *tens = amt;
+        } else if (bill == 5) {
+            *fives = amt;
+        } else {
+            *ones = amt;
+        }
+
+        dollars -= amt * bill;
     }
-
-    return 0;
 }
