d835e1412ac3d0e70dacee8f3503eea22ad8093c
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-20 07:09:46 +0900

    feat: Add progress #5 ‘Question 3, Worksheet 6 solution, CSC 369’

--- csc369/week_3/worksheet_6/question_3.c
+++ csc369/week_3/worksheet_6/question_3.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    int n;
+
     // get arguments
 
-    // create array
+    // convert argument (amount of memory used) from string to integer
+    // calculate the size of array n needed to fill the requested amount of memory
+    n = 10;
 
-    // indefinitely traverse through the array, touching elements
+    // create array of size n (using stack memory)
+    int array[n];
 
-    // while traversing, the following should be displayed
-    //  Current index:
-    //  Time:
-    //  Amount of memory allocated:
-    //  Press Cmd + Z to kill program
+
+    while(1) {
+        for (int i = 0; i < n; i++) {
+            // indefinitely traverse through the array, touching elements
+            array[n] = i % 5;
+            // while traversing, the following should be displayed
+            //  Current index:
+            //  Time:
+            //  Amount of memory allocated:
+            //  Press Cmd + Z to kill program
+        }
+    }
 }
