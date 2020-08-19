1a9aabbe68f16ce79f754f79989f51804bd5d0a8
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-20 07:28:07 +0900

    feat: Add progress #6 ‘Question 3, Worksheet 6 solution, CSC 369’

--- csc369/week_3/worksheet_6/question_3.c
+++ csc369/week_3/worksheet_6/question_3.c
@@ -1,28 +1,37 @@
-#include <stdio.h>
+#include <stdio.h>  // printf
+#include <time.h>   // clock_t, clock, CLOCKS_PER_SEC
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
     int n;
+    clock_t start_t, curr_t;
 
     // get arguments
 
     // convert argument (amount of memory used) from string to integer
     // calculate the size of array n needed to fill the requested amount of memory
     n = 10;
-
     // create array of size n (using stack memory)
     int array[n];
 
+    // setup time
+    start_t = clock();
 
     while(1) {
         for (int i = 0; i < n; i++) {
             // indefinitely traverse through the array, touching elements
             array[n] = i % 5;
-            // while traversing, the following should be displayed
-            //  Current index:
-            //  Time:
-            //  Amount of memory allocated:
-            //  Press Cmd + Z to kill program
+            // display current index:
+            printf("Current index: %d", i);
+            // display time elapsed:
+            curr_t = clock() - start_t;
+            printf("Time elapsed: %f seconds", ((float)curr_t)/CLOCKS_PER_SEC);
+            // display amount of memory allocated:
+
+            printf("Amount of memory allocated: %f MB");
+            // display "press Cmd + Z to kill program"
+
+            // refresh screen
         }
     }
 }
