dd85c4d0665954a8e057de653cd0ad836b647365
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-19 15:38:37 +0900

    feat: Add progress #3 ‘Question 10, Review 4 solution, CSC 209’

--- csc209/week_4/question_10.c
+++ csc209/week_4/question_10.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+void find_closest_flight(int desired_time, int *departure_time, int *arrival_time);
+
+int main(void) {
+
+    int hour, minute, desired_time, departure_time, arrival_time;
+
+    printf("Enter a 24-hour time: ");
+    scanf("%d :%d", &hour, &minute);
+    desired_time = hour * 60 + minute;
+
+    find_closest_flight(desired_time, &departure_time, &arrival_time);
+
+    printf("Closest departure time is ");
+
+    return 0;
+}
+
 void find_closest_flight(int desired_time, int *departure_time, int *arrival_time) {
 
-    int user_time,
-        hour,
-        minute,
-        d1 = 480,
+    int d1 = 480,
         d2 = 583,
         d3 = 679,
         d4 = 767,
@@ -14,26 +28,24 @@ void find_closest_flight(int desired_time, int *departure_time, int *arrival_tim
         d7 = 1140,
         d8 = 1305;
 
-    printf("Enter a 24-hour time: ");
-    scanf("%d :%d", &hour, &minute);
-    user_time = hour * 60 + minute;
-
-    printf("Closest departure time is ");
-
-    if (user_time <= d1 + (d2 - d1) / 2)
-        printf("8:00 a.m., arriving at 10:16 a.m.\n");
-    else if (user_time < d2 + (d3 - d2) / 2)
-        printf("9:43 a.m., arriving at 11:52 a.m.\n");
-    else if (user_time < d3 + (d4 - d3) / 2)
-        printf("11:19 a.m., arriving at 1:31 p.m.\n");
-    else if (user_time < d4 + (d5 - d4) / 2)
+    if (desired_time <= d1 + (d2 - d1) / 2) {
+        *departure_time = 8 * 60;
+        *arrival_time = 10 * 60 + 16;
+    } else if (desired_time < d2 + (d3 - d2) / 2) {
+        *departure_time = 9 * 60 + 43;
+        *arrival_time = 11 * 60 + 52;
+    } else if (desired_time < d3 + (d4 - d3) / 2) {
+        *departure_time = 11 * 60 + 19;
+        *arrival_time = 13 * 60 + 31;
+    } else if (desired_time < d4 + (d5 - d4) / 2) {
         printf("12:47 p.m., arriving at 3:00 p.m.\n");
-    else if (user_time < d5 + (d6 - d5) / 2)
+    } else if (desired_time < d5 + (d6 - d5) / 2) {
         printf("2:00 p.m., arriving at 4:08 p.m.\n");
-    else if (user_time < d6 + (d7 - d6) / 2)
+    } else if (desired_time < d6 + (d7 - d6) / 2) {
         printf("3:45 p.m., arriving at 5:55 p.m.\n");
-    else if (user_time < d7 + (d8 - d7) / 2)
+    } else if (desired_time < d7 + (d8 - d7) / 2) {
         printf("7:00 p.m., arriving at 9:20 p.m.\n");
-    else
+    } else {
         printf("9:45 p.m., arriving at 11:58 p.m.\n");
+    }
 }
