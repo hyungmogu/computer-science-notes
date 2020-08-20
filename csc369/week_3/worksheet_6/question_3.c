12511232608e67053a5a1b6584ff13029a1d712f
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-20 09:34:19 +0900

    feat: Add progress #11 ‘Question 3, Worksheet 6 solution, CSC 369’

--- csc369/week_3/worksheet_6/question_3.c
+++ csc369/week_3/worksheet_6/question_3.c
@@ -1,5 +1,8 @@
 #include <stdio.h>  // printf
+#include <stdlib.h> // atoi
 #include <time.h>   // clock_t, clock, CLOCKS_PER_SEC
+#include <math.h>   // ceil
+#include <string.h> // strncmp
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -16,8 +19,13 @@ int main(int argc, char *argv[]) {
         return(-1);
     }
 
+    if (strncmp(argv[1],"-m", 2) != 0) {
+        perror("Argument must be of format ./question_3 -m <SIZE IN INTEGER>");
+        return(-1);
+    }
 
     // convert argument (requested amount of memory) from string to integer
+    size_in_mb = atoi(argv[2]);
 
     // convert size in megabytes to bytes
     size_in_bytes = size_in_mb * 1000000;
@@ -39,8 +47,7 @@ int main(int argc, char *argv[]) {
             curr_t = clock() - start_t;
             printf("Time elapsed: %f seconds\n", ((float)curr_t)/CLOCKS_PER_SEC);
             // display amount of memory allocated:
-
-            printf("Amount of memory allocated: %f MB\n");
+            printf("Amount of memory allocated: %d MB\n", size_in_mb);
             // display "press Cmd + Z to kill program"
             printf("Press ctrl + z (or cmd + z) to terminate program\n");
             // refresh screen
