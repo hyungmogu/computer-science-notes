4b87ec6021dcbd04b5d763bfb3144ca017894fb7
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-21 05:48:16 +0900

    feat: Add progress #6 ‘Question 8, Worksheet 6 solution, CSC 369’

--- csc369/week_3/worksheet_6/question_8.c
+++ csc369/week_3/worksheet_6/question_8.c
@@ -37,21 +37,29 @@ int main(int argc, char *argv[]) {
     // setup time
     start_t = time(NULL);
 
-    while(1) {
-        for (int i = 0; i < n; i++) {
-            // refresh screen
-            #if defined(__linux__) || defined(__APPLE__)
-                system("clear");
-            #endif
+    // get pid of parent
+    char parent_pid[sizeof(getpid()) + 2];
+    snprintf(parent_pid, sizeof(getpid()) + 2, "%d", getpid());
 
-            // add a child process for displaying memory
-            int rc = fork();
-            if (rc < 0) {
+    while(1) {
+        #if defined(__linux__) || defined(__APPLE__)
+            system("clear");
+        #endif
+
+        // add a child process for displaying memory
+        int rc = fork();
+        if (rc < 0) {
+            perror("ERROR: Fork failed");
+            exit(1);
+        } else if (rc == 0) {
+
+            int rc2 = fork();
+            if (rc2 < 0) {
                 perror("ERROR: Fork failed");
                 exit(1);
-            } else if (rc == 0) {
+            } else if (rc2 == 0) {
                 #if defined(__linux__)
-                    execlp("free", "free", "-m", NULL);
+                    execlp("pmap", "pmap", "-X", parent_pid, NULL);
                 #endif
                 exit(0);
             }
@@ -59,39 +67,22 @@ int main(int argc, char *argv[]) {
             printf("\n");
             wait(NULL);
 
-            int rc2 = fork();
-            if (rc2 < 0) {
-                perror("ERROR: Fork failed");
-                exit(1);
-            } else if (rc2 == 0) {
-                char ppid[sizeof(getppid()) + 1];
-                snprintf(ppid, sizeof(getppid()) + 1, "%d", getppid());
 
-                #if defined(__linux__)
-                    execlp("pmap", "pmap", "-X", ppid, NULL);
-                #endif
-                exit(0);
-            }
+            #if defined(__linux__)
+                execlp("free", "free", "-m", NULL);
+            #endif
+            exit(0);
+        }
 
+        // display "press Cmd + Z to kill program"
+        printf("Press ctrl + z to terminate program\n");
 
-            printf("\n");
-
+        for (int i = 0; i < n; i++) {
             // indefinitely traverse through the array, touching elements
             array[n] = i % 5;
-            // display current index:
-            printf("Current index: %d\n", i);
-            // display time elapsed:
-            curr_t = time(NULL);
-            printf("Time elapsed: %ld seconds\n", (curr_t - start_t));
-            // display amount of memory allocated:
-            printf("Amount of memory allocated: %d MB\n", size_in_mb);
-            // display size n:
-            printf("Size of array: %d \n", n);
-            // display "press Cmd + Z to kill program"
-            printf("Press ctrl + z to terminate program\n");
-
-            sleep(1);
         }
+
+        sleep(1);
     }
 
     return 0;
