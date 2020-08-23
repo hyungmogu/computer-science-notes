7103583ee3e0437f0ad6aa4c2860515da57ec57b
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-23 14:38:49 +0900

    feat: Add progress #3 ‘Question 2, Review 8 Solution, CSC 369’

--- csc209/week_4/review_6/question_2.c
+++ csc209/week_4/review_6/question_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>  // malloc
+#include <string.h>  // strlen
 
 char *duplicate(const char *str);
 
@@ -9,12 +10,16 @@ int main(void) {
     p = duplicate (s);
 
     printf("Duplicate: %s", *p);
+
+    free(p);
     return 0;
 }
 
 
 char *duplicate(const char *str) {
     char *p;
+    const char *q;
+
     int n = strlen(str);
 
     p = (char *)malloc(n + 1);
@@ -23,9 +28,12 @@ char *duplicate(const char *str) {
         return p;
     }
 
-    for (p = str; p < str + n; p++) {
-        *p = *str;
+    q = str;
+    while (q < str + n) {
+        *p++ = *q++;
     }
+
+    *p = '\0';
 
     return p;
 }
