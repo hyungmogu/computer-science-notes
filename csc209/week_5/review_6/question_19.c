2eb02b5188c475f6f5e80210f0f70e77180f4ee6
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 04:01:06 +0900

    feat: Add progress #4 ‘Question 19, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_19.c
+++ csc209/week_5/review_6/question_19.c
@@ -5,18 +5,29 @@ int main(void) {
     int n = 20;
     char smallest_word[n+1], largest_word[n+1], s[n+1];
 
+    smallest_word[0] = '\0';
+    largest_word[0] = '\0';
+
 
     do {
         printf("Enter word: ");
         scanf("%s", s); // ask user to enter words
 
-        if (strcmp(s, largest_word) > 0) { // determine and store the largest word
-            printf("%s\n", s);
+        if (largest_word[0] == '\0') {
             strcpy(largest_word, s);
-        } else if (strcmp(s, smallest_word) < 0) { // determine and store the smallest word
             printf("%s\n", s);
-            strcpy(smallest_word, s);
         }
+
+        if (smallest_word[0] == '\0') {
+            strcpy(smallest_word, s);
+            printf("%s\n", s);
+        }
+
+        // if (strcmp(s, largest_word) > 0) { // determine and store the largest word
+        //     strcpy(largest_word, s);
+        // } else if (strcmp(s, smallest_word) < 0) { // determine and store the smallest word
+        //     strcpy(smallest_word, s);
+        // }
 
     } while (strlen(s) != 4); // stop program when user enters four letter words
 
