61e950ec48ce919458db0fb07f4638c6eed17768
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 06:27:56 +0900

    feat: Add solution ‘Question 21, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_21.c
+++ csc209/week_5/review_6/question_21.c
@@ -1,12 +1,3 @@
-/*********************************************************
- * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
- * By K. N. King                                         *
- * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
- * All rights reserved.                                  *
- * This program may be freely distributed for class use, *
- * provided that this copyright notice is retained.      *
- *********************************************************/
-
 /* deal.c (Chapter 8, page 173) */
 /* Deals a random hand of cards */
 
@@ -22,23 +13,23 @@ int main(void)
 {
   bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
   int num_cards, rank, suit;
-  const char rank_code[] = {'2','3','4','5','6','7','8',
-                            '9','t','j','q','k','a'};
-  const char suit_code[] = {'c','d','h','s'};
+  const char *rank_code[] = {"Two","Three","Four","Five","Six","Seven","Eight",
+                            "Nine","Ten","Jack","Queen","King","Ace"};
+  const char *suit_code[] = {"clubs","diamond","hearts","spades"};
 
   srand((unsigned) time(NULL));
 
   printf("Enter number of cards in hand: ");
   scanf("%d", &num_cards);
 
-  printf("Your hand:");
+  printf("Your hand:\n");
   while (num_cards > 0) {
     suit = rand() % NUM_SUITS;     /* picks a random suit */
     rank = rand() % NUM_RANKS;     /* picks a random rank */
     if (!in_hand[suit][rank]) {
       in_hand[suit][rank] = true;
       num_cards--;
-      printf(" %c%c", rank_code[rank], suit_code[suit]);
+      printf(" %s of %s\n", rank_code[rank], suit_code[suit]);
     }
   }
   printf("\n");
