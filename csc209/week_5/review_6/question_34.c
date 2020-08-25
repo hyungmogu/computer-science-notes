cef2d902c9a14eb2ade51ffac93a983addf98b57
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-26 04:19:22 +0900

    feat: Add progress ‘Question 34, Review 6 Solution, CSC 209’

---
+++ csc209/week_5/review_6/question_34.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+void reverse(char *message);
+
+int main(void) {
+
+    char message[SIZE], c, *p;
+
+    p = &message[0];
+
+    printf("Enter a message: ");
+
+    // fill array
+    while ((c = getchar()) != '\n') {
+        *p++ = c;
+    }
+
+    *p = '\0';
+
+    // reverse characters in array
+    reverse(message);
+
+    printf("Reversal is: %s", message);
+
+    return 0;
+}
+
+void reverse (char *message) {
+    char *q = message, *r = message, temp;
+
+    while (*r++)
+        ;
+
+    r--;
+    // while (q < r) {
+    //     if (*r == '\0') {
+    //         r--;
+    //         continue;
+    //     }
+
+    //     temp = *q;
+    //     *q++ = *r;
+    //     *r-- = temp;
+    // }
+}
