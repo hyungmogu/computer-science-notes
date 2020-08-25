c11ab246904a7c5466009948803b125b58bd0afc
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 11:04:39 +0900

    feat: Add progress #3 ‘Question 28, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_28.c
+++ csc209/week_5/review_6/question_28.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define SIZE 50
 
@@ -6,19 +7,33 @@ void reverse_name(char *name);
 
 int main(void) {
 
-    char first_name[SIZE], last_name[SIZE], full_name[SIZE];
+    char name[SIZE];
 
     printf("Enter a first and last name: ");
-    scanf("%s", first_name);
-    scanf("%s", last_name);
+    scanf("%s", name);
+    strcat(name, ' ');
+    scanf("%s", name);
 
-    strcat(full_name, first_name);
-    strcat(full_name, " ");
-    strcat(full_name, last_name);
+    reverse_name(name);
 
+    printf("%s\n", name);
     return 0;
 }
 
 void reverse_name(char *name) {
+    int n = strlen(name);
+    char first_name[n], last_name[n], *p;
 
+    bool is_first_name = true;
+
+    for (p = name; *p != '\0'; p++) {
+        if (*p == ' ') {
+            is_first_name = false;
+            continue;
+        }
+
+        if (is_first_name) {
+            *first_name = *p;
+        }
+    }
 }
