81e672986df39eac3c94d6ad64b3c2931b39f561
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 09:58:14 +0900

    feat: Add progress #2 ‘Question 27, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_27.c
+++ csc209/week_5/review_6/question_27.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define SIZE 100
+
 int compute_vowel_count(const char *sentence);
 
 int main(void) {
 
-    char c;
-    int vowels = 0;
+    char ch, sentence[SIZE], *p = sentence;
+    int vowels;
 
     printf("Enter a sentence: ");
-
-    while ((c = getchar()) != '\n') {
-        switch (toupper(c)) {
-            case 'A': case 'E': case 'I': case 'O': case 'U':
-                vowels++;
-                break;
-            default:
-                break;
+    while ((ch = getchar()) != EOF) {
+        if (ch == '\n') {
+            break;
         }
+
+        *p++ = ch;
     }
+
+    vowels = compute_vowel_count(sentence);
+
     printf("Your sentence contains %d vowels.\n", vowels);
     return 0;
 }
 
 int compute_vowel_count(const char *sentence) {
+    int count = 0;
 
+    do {
+        switch (toupper(*sentence)) {
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                count++;
+                break;
+            default:
+                break;
+        }
+    } while (*sentence++);
+
+    return count;
 }
