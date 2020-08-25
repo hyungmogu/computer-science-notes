c4ff334c2e0d3f96f9cf3893665fed20643556c5
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-26 05:28:20 +0900

    feat: Add solution ‘Question 36, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_36.c
+++ csc209/week_5/review_6/question_36.c
@@ -2,5 +2,18 @@
 
 int main(void) {
 
+    char *months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November",
+        "December"
+    };
+
+    int month, day, year;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%2d/%2d/%4d", &month, &day, &year);
+
+    printf("You entered the date %s %02d, %04d\n", months[month - 1], day, year);
+
     return 0;
 }
