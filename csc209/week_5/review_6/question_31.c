3d88823cbabb9b97385b5cb1a140c18b55a84c2b
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 23:38:03 +0900

    feat: Add progress #3 ‘Question 31, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_31.c
+++ csc209/week_5/review_6/question_31.c
@@ -8,7 +8,7 @@ int main(void) {
 
     char c, sentence[MAX_VALUE] = {0};
 
-    int i, n, length;
+    int i, shift, length;
 
     printf("Enter message to be encrypted: ");
     for (i = 0, length = 0; (c = getchar()) != '\n' && i < MAX_VALUE; i++) {
@@ -17,22 +17,22 @@ int main(void) {
     }
 
     printf("Enter shift amount (1-25): ");
-    scanf("%d", &n);
+    scanf("%d", &shift);
 
-    printf("Encrypted message: ");
-    for (i = 0; i < length; i++) {
-        if (sentence[i] >= 'A' && sentence[i] <= 'Z')
-            sentence[i] = ((sentence[i] - 'A') + n) % 26 + 'A';
-        else if (sentence[i] >= 'a' && sentence[i] <= 'z')
-            sentence[i] = ((sentence[i] - 'a') + n) % 26 + 'a';
+    printf("Encrypted message: %s", encrypt(message, shift));
 
-        putchar(sentence[i]);
-    }
     printf("\n");
 
     return 0;
 }
 
 void encrypt(char *message, int shift) {
+    for (i = 0; i < length; i++) {
+        if (message[i] >= 'A' && message[i] <= 'Z')
+            message[i] = ((message[i] - 'A') + shift) % 26 + 'A';
+        else if (sentence[i] >= 'a' && message[i] <= 'z')
+            message[i] = ((message[i] - 'a') + shift) % 26 + 'a';
 
+        putchar(message[i]);
+    }
 }
