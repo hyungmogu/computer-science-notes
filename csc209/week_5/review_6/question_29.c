c22da5524b2db242f77b7b5d6841ac5c5e092608
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-25 14:35:49 +0900

    feat: Add solution ‘Question 29, Review 6 Solution, CSC 209’

--- csc209/week_5/review_6/question_29.c
+++ csc209/week_5/review_6/question_29.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
+#define SIZE 100
+
+double compute_average_word_length(const char *sentence);
+
 int main(void) {
 
-    char c;
+    char sentence[SIZE], *p, c;
     double length = 0.0;
-    int words = 1;
 
     printf("Enter a sentence: ");
+    for (p = sentence; (c = getchar()) != '\n'; p++) {
+        *p = c;
+    }
 
-    while ((c = getchar()) != '\n') {
-        if (c == ' ')
+    *p = '\0';
+
+    length = compute_average_word_length(sentence);
+    printf("Average word length: %.2f\n", length);
+    return 0;
+}
+
+double compute_average_word_length(const char *sentence) {
+    int words = 0, length = 0;
+
+    for (; *sentence != '\0'; sentence++) {
+        if (*sentence == ' ')
             words++;
         else
             length++;
     }
 
-    printf("Average word length: %.1f\n", length / words);
-    return 0;
+    if (length > 0 && *sentence == '\0') {
+        words++;
+    }
+
+    return (double)length/words;
 }
