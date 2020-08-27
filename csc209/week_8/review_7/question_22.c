d2b25ec6a33e43772355e065f6735b17d7869686
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-28 00:46:04 +0900

    feat: Add progress #2 ‘Question 23, Review 7 Solution, CSC 209’

--- csc209/week_8/review_7/question_22.c
+++ csc209/week_8/review_7/question_22.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+struct dialing_code {
+    char *country;
+    int code;
+};
+
 int main(void) {
+    const struct dialing_code country_codes[] = {
+        {"Argentina", 54}, {"Bangladesh", 880},
+        {"Brazil", 55}, {"Burma (Myanmar)", 95},
+        {"China", 86}, {"Colombia", 57},
+        {"Congo, Dem. Rep. of", 243}, {"Egypt", 20},
+        {"Ethiopia", 251}, {"France", 33},
+        {"Germany", 49}, {"India", 91},
+        {"Indonesia", 62}, {"Iran", 98},
+        {"Italy", 39}, {"Japan", 81},
+        {"Mexico", 52}, {"Nigeria", 234},
+        {"Pakistan", 92}, {"Philippines", 63},
+    };
+
     return 0;
 }
