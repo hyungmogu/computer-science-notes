cdd411c5b8259f0363eb3c7aa44b35519dbe9023
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-28 01:02:38 +0900

    feat: Add solution ‘Question 23, Review 7 Solution, CSC 209’

--- csc209/week_8/review_7/question_22.c
+++ csc209/week_8/review_7/question_22.c
@@ -17,7 +17,24 @@ int main(void) {
         {"Italy", 39}, {"Japan", 81},
         {"Mexico", 52}, {"Nigeria", 234},
         {"Pakistan", 92}, {"Philippines", 63},
+        {"Poland", 48}, {"Russia", 7},
+        {"South Africa", 27}, {"South Korea", 82},
+        {"Spain", 34}, {"Sudan", 249},
+        {"Thailand", 66}, {"Turkey", 90},
+        {"Ukraine", 380}, {"United Kingdom", 44},
+        {"United States", 1}, {"Vietnam", 84}
     };
+
+    int dialing_code, n = sizeof(country_codes)/sizeof(dialing_code);
+
+    printf("Please enter the dialing code: ");
+    scanf("%d", &dialing_code);
+
+    for(int i = 0; i < n; i++) {
+        if (country_codes[i].code == dialing_code) {
+            printf("%s\n", country_codes[i].country);
+        }
+    }
 
     return 0;
 }
