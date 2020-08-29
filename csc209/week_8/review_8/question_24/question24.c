4c8e60b616780e7918387f2773e9e9ae871ec42b
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-30 08:47:35 +0900

    feat: Add progress #2 ‘Question 24, Review 8 Solution, CSC 209’

---
+++ csc209/week_8/review_8/question_24/question24.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readline.h"
+
+#define MAX_LENGTH 20
+
+struct node {
+    char word[MAX_LENGTH];
+};
+
+int main(void) {
+    int arr_size = 10;
+    struct node *words;
+
+    // create dynamically allocated array
+    words = malloc(arr_size * sizeof(struct node));
+
+    for(;;;) {
+        //
+
+        // readline
+
+        // store words
+        if () {
+
+        }
+    }
+
+    return 0;
+}
