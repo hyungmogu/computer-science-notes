9f89b2f0a1b1dcaa175c4a86d366e3a6cb3d7294
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-30 04:47:25 +0900

    feat: Add progress #7 ‘Question 23, Review 8 Solution, CSC 209’

--- csc209/week_8/review_8/question_23/question23.c
+++ csc209/week_8/review_8/question_23/question23.c
@@ -68,17 +68,20 @@ void write_line(void)
 {
   int extra_spaces, spaces_to_insert, i, j;
 
+  struct word *p = line, *cur;
   extra_spaces = MAX_LINE_LEN - line_len;
-  for (i = 0; i < line_len; i++) {
-    if (line[i] != ' ')
-      putchar(line[i]);
-    else {
-      spaces_to_insert = extra_spaces / (num_words - 1);
-      for (j = 1; j <= spaces_to_insert + 1; j++)
-        putchar(' ');
-      extra_spaces -= spaces_to_insert;
-      num_words--;
-    }
+
+  while (p != NULL) {
+    cur = p;
+    puts(cur->value);
+    p = p->next;
+
+    spaces_to_insert = extra_spaces / (num_words - 1);
+    for (j = 1; j <= spaces_to_insert + 1; j++)
+      putchar(' ');
+    extra_spaces -= spaces_to_insert;
+    num_words--;
+
   }
   putchar('\n');
 }
