4e4be0e4b89abe12071ac2fbd559ef8e45166251
Hyungmo Gu <guhyungm7@gmail.com>
2020-08-31 05:47:21 +0900

    feat: Add progress #9 ‘Question 26, Review 8 Solution, CSC 209’

--- csc209/week_8/review_8/question_26/question26.c
+++ csc209/week_8/review_8/question_26/question26.c
@@ -20,11 +20,7 @@ int main(void)
   char day_str[3], msg_str[MSG_LEN+1];
   int day, i, j, num_remind = 0, string_length = 0;
 
-  struct vstring reminders[MAX_REMIND], *p;
-
-  for(p = reminders; p < reminders + MAX_REMIND; p++) {
-    p = malloc(sizeof(struct vstring) + string_length - 1);
-  }
+  struct vstring *reminders[MAX_REMIND], *p = reminders;
 
   for (;;) {
     if (num_remind == MAX_REMIND) {
@@ -40,26 +36,26 @@ int main(void)
     read_line(msg_str, MSG_LEN);
 
     for (i = 0; i < num_remind; i++)
-      if (strcmp(day_str, reminders[i].chars) < 0)
+      if (strcmp(day_str, (*reminders[i]).chars) < 0)
         break;
     for (j = num_remind; j > i; j--)
-      strcpy(reminders[j].chars, reminders[j-1].chars);
+      reminders[j] = reminders[j-1];
 
-    reminders[i] = malloc(2 + strlen(msg_str) + 1);
+    reminders[i] = malloc(sizeof(struct vstring) + string_length - 1);
     if (reminders[i] == NULL) {
       printf("-- No space left --\n");
       break;
     }
 
-    strcpy(reminders[i], day_str);
-    strcat(reminders[i], msg_str);
+    strcpy((*reminders[i]).chars, day_str);
+    strcat((*reminders[i]).chars, msg_str);
 
     num_remind++;
   }
 
   printf("\nDay Reminder\n");
   for (i = 0; i < num_remind; i++)
-    printf(" %s\n", reminders[i]);
+    printf(" %s\n", (*reminders[i]).chars);
 
   return 0;
 }
