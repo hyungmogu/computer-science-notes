910d2f6c1c02649f93b5cd95dbbc9a6d48650eb6
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-11 04:36:32 +0900

    feat: progress #3 ‘Question 6, Review 8_2, CSC 209’

---
+++ csc209/week_8/review_8/02/question_6.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node *next;
+};
+
+struct node *delete_from_list(struct node *list, int n);
+
+int main(void)
+{
+    struct node *l, *l1, *l2, *l3, *l4, *p;
+
+    l = malloc(sizeof(struct node));
+    l1 = malloc(sizeof(struct node));
+    l2 = malloc(sizeof(struct node));
+    l3 = malloc(sizeof(struct node));
+    l4 = malloc(sizeof(struct node));
+
+    l->value = 1;
+    l->next = &l1;
+    l1->value = 2;
+    l1->next = &l2;
+    l2->value = 3;
+    l2->next = &l3;
+    l3->value = 4;
+    l3->next = &l4;
+    l4->value = 5;
+
+    delete_from_list(&l, 3);
+
+    for()
+
+    return 0;
+}
+
+struct node *delete_from_list(struct node *list, int n)
+{
+    struct node *cur = list, *temp;
+
+    if (cur->value == n) {
+        list = cur->next;
+        return list;
+    }
+
+    for (cur = list;
+            cur != NULL;
+            cur = cur -> next) {
+
+            if (cur->next != NULL && cur->next->value == n) {
+            break;
+            }
+    }
+
+    if (cur == NULL) {
+        return list;
+    }
+
+    temp = cur->next;
+    cur->next = cur->next->next;
+
+    free(temp);
+    return list;
+}
