631e850184aaac9054a13f8c604e8ba6653d178a
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-11 13:34:16 +0900

    feat: Add progress #4 ‘Question 19, Review 8_2, CSC 209’

--- csc209/week_8/review_8/02/question_19/question_19.c
+++ csc209/week_8/review_8/02/question_19/question_19.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void new_cmd(void);
 void open_cmd(void);
-void close_cmd(void);
 void close_cmd(void);
 void close_all_cmd(void);
 void save_cmd(void);
@@ -11,6 +12,8 @@ void save_as_cmd(void);
 void save_all_cmd(void);
 void print_cmd(void);
 void exit_cmd(void);
+
+void run_cmd(const char *cmd);
 
 struct {
     char *cmd_name;
@@ -29,6 +32,8 @@ struct {
 
 int main(void)
 {
+    run_cmd("new");
+    run_cmd("OPEN");
     return 0;
 }
 
@@ -36,17 +41,58 @@ int main(void)
 void run_cmd(const char *cmd)
 {
     int cmd_cnt = sizeof(file_cmd)/sizeof(file_cmd[0]);
-    char cmd_cpy[21];
+    char cmd_cpy[21], *p;
 
     strcpy(cmd_cpy, cmd);
     if (strlen(cmd) == 21) {
-        cmd_cpy[21] = '\0';
+        cmd_cpy[20] = '\0';
+    }
+
+    for (p = cmd_cpy; *p != '\0'; p++)
+    {
+        *p = tolower(*p);
     }
 
     for (int i = 0; i < cmd_cnt; i++) {
-        if (strcmp((file_cmd[i]).cmd_name, cmd) == 0) {
+        if (strcmp((file_cmd[i]).cmd_name, cmd_cpy) == 0) {
             return (file_cmd[i]).cmd_pointer();
         }
     }
 }
 
+void new_cmd(void)
+{
+    printf("new_cmd\n");
+}
+void open_cmd(void)
+{
+    printf("open_cmd\n");
+}
+void close_cmd(void)
+{
+    printf("close_cmd\n");
+}
+void close_all_cmd(void)
+{
+    printf("close_all_cmd\n");
+}
+void save_cmd(void)
+{
+    printf("save_cmd\n");
+}
+void save_as_cmd(void)
+{
+    printf("save_as_cmd\n");
+}
+void save_all_cmd(void)
+{
+    printf("save_all_cmd\n");
+}
+void print_cmd(void)
+{
+    printf("print_cmd\n");
+}
+void exit_cmd(void)
+{
+    printf("exit_cmd\n");
+}
