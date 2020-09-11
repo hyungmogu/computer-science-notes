3cbb233631c5ea0c09ba30e98c68e1d50695c75a
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-11 09:55:55 +0900

    feat: Add solution ‘Question 14, Review 8_2, CSC 209’

--- csc209/week_8/review_8/02/question_14/question_14.c
+++ csc209/week_8/review_8/02/question_14/question_14.c
@@ -11,21 +11,30 @@ struct node *delete_from_list(struct node **list, int n);
 
 int main(void)
 {
-    struct node *n1, *n2, *n3, *res;
+    struct node *n1, *n2, *n3, *res, *p, *to_be_freeed;
 
     n1 = malloc(sizeof(struct node));
     n2 = malloc(sizeof(struct node));
     n3 = malloc(sizeof(struct node));
 
     n1->value = 1;
-    n1->next = &n2;
+    n1->next = n2;
     n2->value = 2;
-    n2->next = &n3;
-    n3->value = 2;
+    n2->next = n3;
+    n3->value = 3;
+
+    res= delete_from_list(&n1, 3);
 
     for (p = res; p != NULL; p=p->next) {
         printf("%d\n", p->value);
     }
+
+    p = res;
+    while(p != NULL) {
+        to_be_freeed = p;
+        p = p->next;
+        free(p);
+    };
 
     return 0;
 }
@@ -34,22 +43,22 @@ struct node *delete_from_list(struct node **list, int n)
 {
     struct node *cur, *prev;
 
-    for (cur = list, prev = NULL;
+    for (cur = *list, prev = NULL;
             cur != NULL && cur->value != n;
             prev = cur, cur = cur->next)
 
             ;
 
     if (cur == NULL) {
-        return list;
+        return (*list);
     }
 
     if (prev == NULL) {
-        list = list->next;
+        *list = (*list)->next;
     } else {
         prev->next = cur->next;
     }
 
     free(cur);
-    return list;
+    return (*list);
 }
