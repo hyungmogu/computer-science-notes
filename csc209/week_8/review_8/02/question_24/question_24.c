bec64c842abdb2ca0439906ae1d61882e5a7602c
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-12 07:21:45 +0900

    feat: Add progress #4 ‘Question 24 Review 8_2, CSC 209’

--- csc209/week_8/review_8/02/question_24/question_24.c
+++ csc209/week_8/review_8/02/question_24/question_24.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MAX_SIZE 20
+#define MAX_SIZE 21
+
+void read_word(char word[]);
 
 int main(void) {
 
-    char *words[MAX_SIZE], ch, word[MAX_SIZE];
+    char *words, ch, word[MAX_SIZE];
 
     while(true) {
         printf("Enter word:");
         // read word
         read_word(word);
-        ch = getchar();
-        printf("%c", ch);
+        printf("%s", word);
 
-        if (ch == '\n') {
+        if (word == "") {
             printf("I am here");
         }
 
@@ -23,4 +24,29 @@ int main(void) {
     }
 
     return 0;
+}
+
+void read_word(char word[]) {
+    char ch;
+    int i = 0;
+
+    // 1  if received char is a space, then continue
+    // 2. if first char of read_word is \n, then add to word and exit
+    // 3. if first char of read_word is not \n, then add to word
+    // 4. at the end, add '\0'
+
+    while(true) {
+        ch = getchar();
+
+        if (ch == ' ') {
+            continue;
+        }
+
+        word[i++] = ch;
+
+        if (ch == '\n') {
+            word[i] = '\0';
+            break;
+        }
+    }
 }
