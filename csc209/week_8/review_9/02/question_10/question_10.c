026887ba9ccfbf6ee1ba52a35d64060e37c6950f
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 06:41:02 +0900

    feat: Add solution ‘Question 10, Review 9_2, CSC 209’

--- csc209/week_8/review_9/02/question_10/question_10.c
+++ csc209/week_8/review_9/02/question_10/question_10.c
@@ -11,14 +11,14 @@ int main() {
 int reverse_bits(unsigned int n)
 {
     int max_bits = (sizeof(int) * 4);
-    unsigned int bits[max_bits], res;
+    unsigned int bits[max_bits], res = 0;
 
     for (int i = 0; i < max_bits; i++) {
         bits[i] = (n >> i) & 1;
     }
 
     for (int i = max_bits - 1; i >= 0; i--) {
-        res |= bits[i - (max_bits - 1)] << i;
+        res |= bits[(max_bits - 1) - i] << i;
     }
 
     return res;
