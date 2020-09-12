50eff5edae916fa90e032d6991d10c47c51cb84e
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 04:39:42 +0900

    feat: Add solution ‘Question 7, Review 9_2, CSC 209’

--- csc209/week_8/review_9/02/question_7/question_7.c
+++ csc209/week_8/review_9/02/question_7/question_7.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int swap_bytes(int i);
+int rotate_left(int hex, int n);
+int rotate_right(int hex, int n);
 
 int main() {
-    printf("rotated left: %hx", rotate_left(0x12345678));
+    printf("rotated left: %x", rotate_left(0x12345678, 4));
+    printf("rotated left: %x", rotate_right(0x12345678, 4));
     return 0;
 }
 
-int rotate_left(int i) {
-    int res;
+int rotate_left(int hex, int n) {
+    int res = hex;
+
+    for (int i = 0; i < n; i++) {
+        res = (res >> 4) | (res << 28);
+    }
+
+    return res;
+}
+
+int rotate_right(int hex, int n) {
+    int res = hex;
+
+    for (int i = 0; i < n; i++) {
+        res = (res << 4) | (res >> 28);
+    }
 
     return res;
 }
