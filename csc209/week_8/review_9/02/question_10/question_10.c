1a378bfbc9365b52c08dda79c64edf424a3cd05b
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 06:23:38 +0900

    feat: Add progress ‘Question 10, Review 9_2, CSC 209’

---
+++ csc209/week_8/review_9/02/question_10/question_10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int reverse_bits(unsigned int n);
+
+int main() {
+    return 0;
+}
+
+int reverse_bits(unsigned int n)
+{
+    int res;
+    return res;
+}
