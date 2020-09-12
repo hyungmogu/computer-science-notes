0d9d6496c84ed10c3d53852739ad702d87f93c0a
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 05:52:39 +0900

    feat: Add progress #2 ‘Question 9, Review 9_2, CSC 209’

--- csc209/week_8/review_9/02/question_9/question_9.c
+++ csc209/week_8/review_9/02/question_9/question_9.c
@@ -4,15 +4,19 @@
 int count_ones(unsigned char ch);
 
 int main() {
+    printf("%x\n", '1');
+    printf("%d\n", count_ones('1'));
     return 0;
 }
 
 int count_ones (unsigned char ch)
 {
     int count = 0;
-    for (int i = 0; i < sizeof(unsigned char); i++) {
+    for (int i = 0; i < 8; i++) {
         if ((ch >> i) & 1) {
             count += 1;
         }
     }
+
+    return count;
 }
