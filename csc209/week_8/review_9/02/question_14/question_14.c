0f7601ff7f631786a287267035e67c9baa494e6a
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 08:15:47 +0900

    feat: Add progress #2 ‘Question 14, Review 9_2, CSC 209’

--- csc209/week_8/review_9/02/question_14/question_14.c
+++ csc209/week_8/review_9/02/question_14/question_14.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ieee_float {
-
+union ieee_float {
+    float value;
+    struct {
+        unsigned int fraction: 23;
+        unsigned int exponent: 8;
+        unsigned int sign: 1;
+    } parts;
 };
 
 int main() {
+    union ieee_float f1;
+
+    f1.parts.fraction = 20;
+    f1.parts.exponent = 8;
+
+    printf("%f\n", f1.value);
     return 0;
 }
