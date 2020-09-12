a88faebe6cd041d66e8bacc9656f018df675be69
Hyungmo Gu <guhyungm7@gmail.com>
2020-09-13 04:28:46 +0900

    feat: Add progress ‘Question 7, Review 9_2, CSC 209’

---
+++ csc209/week_8/review_9/02/question_7/question_7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap_bytes(int i);
+
+int main() {
+    printf("rotated left: %hx", rotate_left(0x12345678));
+    return 0;
+}
+
+int rotate_left(int i) {
+    int res;
+
+    return res;
+}
